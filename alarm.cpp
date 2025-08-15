c7966436b79a4ebda94a00cb859c51e6c88af264
Hariprasath Krishnamoorthi <hariprasath.krishnamoorthi@psiog.com>
2025-08-15 12:24:11 -0700

    initial commit

---
+++ alarm.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<thread>
+#include<chrono>
+using namespace std;
+
+int main(){
+    cout << "Alarm Clock Similation" << endl;
+
+    while (true){
+        Cout<<"\n Alarm Rings!" << endl;
+        
+    }
+}
